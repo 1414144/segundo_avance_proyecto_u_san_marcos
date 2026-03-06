384f83ef1893c7b29f1b36a5782ac4534f96510a
1414144 <107646325+1414144@users.noreply.github.com>
2026-03-05 22:15:30 -0600

    Complete consultar_producto and update menu logic
    Completed the 'consultar_producto' function and updated menu options.

--- index.py
+++ index.py
@@ -1,3 +1,46 @@
+#librerias
+from datetime import datetime
+
+
+#funciones 
+def agregar_producto():
+    global total_perdidas
+    print("\n--- Agregar Producto ---")
+
+    nombre = input("Nombre: ")
+    cantidad = int(input("Cantidad: "))
+    precio = float(input("Precio: "))
+    fecha_texto = input("Fecha vencimiento (DD/MM/AAAA): ")
+
+    fecha_vencimiento = datetime.strptime(fecha_texto, "%d/%m/%Y")
+    fecha_hoy = datetime.now()
+
+    estado = "DISPONIBLE"
+
+    if fecha_vencimiento < fecha_hoy:
+        estado = "VENCIDO"
+        perdida = cantidad * precio
+        total_perdidas = total_perdidas + perdida
+
+    producto = {
+        "nombre": nombre,
+        "cantidad": cantidad,
+        "precio": precio,
+        "fecha": fecha_vencimiento,
+        "estado": estado
+    }
+
+
+def mostrar_perdidas():
+    print("\nTotal de perdidas:", total_perdidas)
+    print()
+# este esta incompleto conpletalo 
+def consultar_producto():
+    if len(inventario) == 0:
+        print("\nNo hay productos.")
+        print()
+        return
+
 def menu():
     opcion = 0
 
@@ -12,18 +55,21 @@ def menu():
         opcion = int(input("Opcion: "))
 
         if opcion == 1:
-            agregar_producto()
+          agregar_producto()
         elif opcion == 2:
-            mostrar_perdidas()
+#            mostrar_perdidas()
         elif opcion == 3:
             consultar_producto()
-        elif opcion == 4:
-            borrar_producto()
-        elif opcion == 5:
+#        elif opcion == 4:
+#            borrar_producto()
+#        elif opcion == 5:
             print("Saliendo...")
         else:
             print("Opcion invalida")
             print()
+
+
+
 
 
 menu()
